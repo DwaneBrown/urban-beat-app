afb3bef3cddd1d491ab003b44e0e7249025efcb3
AmosMwangi <amosmwangi101@gmail.com>
2019-10-20 23:09:25 +0300

    made music app

---
+++ scripts.js
@@ -0,0 +1,32 @@
+    window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-44437876-1');
+
+    $('#playlist').musicPlaylist({
+ 
+        //Main Options
+        playerWidth: '40%',
+        playerColor: '#000',
+        playerOpacity: '0.8',
+        iconColor: '#fff',
+        slidersBackgroundColor: '#333333',
+        slidersValueColor: '#FF0000',
+        playerSize: 'big',
+        titleSize: '15px',
+        titleColor: '#fff',
+        titleFontFamily: 'arial',
+        startingVolume: '100',
+ 
+      
+        showPlayList: 'show', 
+        trackImageSize: '40px', 
+        trackTextColor: '#fff',
+        trackTextSize: '14px',
+        trackFontFamily: 'arial',
+        playlistColor: '#4c4c4c',
+        activeTrackBackground: '#525252',
+        activeTrackTextColor: '#fff',
+        separatorColor: '#000', 
+    });
